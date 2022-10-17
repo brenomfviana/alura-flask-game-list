a8c3c2d58d7419c0f198c70ede41c16507507ba0
Breno Viana <brenomfviana@gmail.com>
2022-10-17 16:13:57 -0300

    adds game registration page

--- src/game.py
+++ src/game.py
@@ -1,5 +1,11 @@
 class Game:
-    def __init__(self, name: str, category: str, platform: str) -> None:
+    def __init__(
+        self,
+        *,
+        name: str,
+        category: str,
+        platform: str,
+    ) -> None:
         self.name = name
         self.category = category
         self.platform = platform
--- src/main.py
+++ src/main.py
@@ -1,38 +1,40 @@
-from flask import Flask, render_template
+from platform import platform
+from flask import Flask, render_template, request
 from game import Game
 
 app = Flask(__name__)
 
+games = [
+    Game(
+        name="God of War",
+        category="Rack'n Slash",
+        platform="PS2",
+    ),
+    Game(
+        name="Mortal Combat",
+        category="Fighting",
+        platform="PS3",
+    ),
+    Game(
+        name="Crash Bandicoot",
+        category="Adventure",
+        platform="PS1",
+    ),
+    Game(
+        name="Valorant",
+        category="FPS",
+        platform="PC",
+    ),
+    Game(
+        name="Tetris",
+        category="Puzzle",
+        platform="Atari",
+    ),
+]
+
 
 @app.route("/")
-def run():
-    games = [
-        Game(
-            name="God of War",
-            category="Rack'n Slash",
-            platform="PS2",
-        ),
-        Game(
-            name="Mortal Combat",
-            category="Fighting",
-            platform="PS3",
-        ),
-        Game(
-            name="Crash Bandicoot",
-            category="Adventure",
-            platform="PS1",
-        ),
-        Game(
-            name="Valorant",
-            category="FPS",
-            platform="PC",
-        ),
-        Game(
-            name="Tetris",
-            category="Puzzle",
-            platform="Atari",
-        ),
-    ]
+def index():
     return render_template(
         "game_list.html",
         title="Jogos",
@@ -40,4 +42,35 @@ def run():
     )
 
 
-app.run()
+@app.route("/new")
+def new():
+    return render_template(
+        "new_game.html",
+        title="New Game",
+    )
+
+
+@app.route(
+    "/create",
+    methods=[
+        "POST",
+    ],
+)
+def create():
+    name = request.form["name"]
+    category = request.form["category"]
+    platform = request.form["platform"]
+    game = Game(
+        name=name,
+        category=category,
+        platform=platform,
+    )
+    games.append(game)
+    return render_template(
+        "game_list.html",
+        title="Jogos",
+        games=games,
+    )
+
+
+app.run(debug=True)
