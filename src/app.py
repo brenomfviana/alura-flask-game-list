cbc4bac7ce26b0a843d3929b1a5d908a6a5c7dc4
Breno Viana <brenomfviana@gmail.com>
2022-10-19 14:24:07 -0300

    formats code

--- src/app.py
+++ src/app.py
@@ -1,13 +1,12 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from flask import Flask
 
 app = Flask(__name__)
 
 app.config.from_pyfile("config.py")
 
 db = SQLAlchemy(app)
-
 
 if __name__ == "__main__":
     from apis.views import *
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,4 +1,4 @@
 from .auth import AuthService
-from .route import RouteService
 from .game import GameService
+from .route import RouteService
 from .user import UserService
